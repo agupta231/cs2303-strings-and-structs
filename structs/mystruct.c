081cf474bdec2c5912e6af69a451ead6e1d64001
Ankur Gupta <rukna1000@gmail.com>
2018-09-13 09:29:51 -0400

    Add default files for the project

---
+++ structs/mystruct.c
@@ -0,0 +1,24 @@
+/** mystruct.c
+ * @author Mike Ciaraldi
+*/
+
+
+#include <string.h>
+#include <stdlib.h>
+#include "mystruct.h"
+
+/** Allocates an Employee struct and fills in its fields
+ * @param birth Year the emploee was born.
+ * @param start Year the employee started with the company.
+ * @param name String containing employee's name
+ * @return Pointer to Employee struct, newly allocated from the heap.
+ */
+struct Employee* makeEmployee(int birth, int start, const char *name) {
+	return NULL; // Replace this with a pointer to the allocated struct
+}
+
+/**
+ *
+ */
+void printEmployee(struct Employee *e) {
+}
